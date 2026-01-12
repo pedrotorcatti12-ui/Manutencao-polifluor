860264dbfe5c3e2b7de15be53548488fd125c689
pedrotorcatti12-ui <pedrotorcatti12@gmail.com>
2026-01-12 02:32:40 -0300

    Delete ToastContext.tsx

--- ToastContext.tsx
+++
@@ -1,7 +0,0 @@
-import React, { createContext, useContext, ReactNode } from 'react';
-export const ToastProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const showToast = (msg: string, type: string) => alert(msg);
-    return <ToastContext.Provider value={{ showToast }}>{children}</ToastContext.Provider>;
-};
-const ToastContext = createContext({ showToast: (msg: string, type: string) => {} });
-export const useToast = () => useContext(ToastContext);
