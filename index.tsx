8acd5d9009e180d27fa25c3b44542a84c2316048
pedrotorcatti12-ui <pedrotorcatti12@gmail.com>
2026-01-12 01:59:07 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AppProvider } from './contexts/AppContext';
+import { DataProvider } from './contexts/DataContext';
+import { ToastProvider } from './contexts/ToastContext';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+if (!rootElement) throw new Error("Root element not found");
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <AppProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </AppProvider>
+    </ToastProvider>
   </React.StrictMode>
 );
